1d5c0331d0dea119242003eeaed65a61db2c66e5
Beela303 <devbeela@gmail.com>
2025-07-24 00:04:19 +0100

    post, category and comment models

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin) :
+    list_display = ('title', 'content', 'status', 'created_on', 'edited_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepoluted_fields = {'slug': ('title',)}
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Published")
+)
+
+class Category(models.Model) :
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model) :
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    slug = models.SlugField(max_length=500, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    categories = models.ManyToManyField(Category, related_name ="posts")
+
+    class Meta :
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model) :
+    author = models.CharField(max_length=60)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
+    
